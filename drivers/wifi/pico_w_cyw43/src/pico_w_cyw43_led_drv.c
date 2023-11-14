af9fa65f5a1f9f2d8d04142d2330ca569822c395
LIU, CHAUNCY <chauncy@beechwoods.com>
2023-11-14 11:03:04 -0500

    change functions to include cyw43

--- drivers/wifi/pico_w_cyw43/src/pico_w_cyw43_led_drv.c
+++ drivers/wifi/pico_w_cyw43/src/pico_w_cyw43_led_drv.c
@@ -14,29 +14,29 @@
 
 #define DT_DRV_COMPAT infineon_cyw43_led
 
-static int cyw43_led_on(const struct device *dev, uint32_t led)
+static int rpi_cyw43_led_on(const struct device *dev, uint32_t led)
 {
     if (led == 0) return cyw43_gpio_set(&cyw43_state, 0, true);
     return -ENOTSUP;
 }
 
-static int cyw43_led_off(const struct device *dev, uint32_t led)
+static int rpi_cyw43_led_off(const struct device *dev, uint32_t led)
 {
     if (led == 0) return cyw43_gpio_set(&cyw43_state, 0, false);
     return -ENOTSUP;
 }
 
-static int cyw43_led_init(const struct device *dev)
+static int rpi_cyw43_led_init(const struct device *dev)
 {
     return 0;
 }
 
-static const struct led_driver_api cyw43_led_api = {
-    .on = cyw43_led_on,
-    .off = cyw43_led_off,
+static const struct led_driver_api rpi_cyw43_led_api = {
+    .on = rpi_cyw43_led_on,
+    .off = rpi_cyw43_led_off,
 };
 
-DEVICE_DT_INST_DEFINE(0, cyw43_led_init, NULL, NULL,
+DEVICE_DT_INST_DEFINE(0, rpi_cyw43_led_init, NULL, NULL,
               NULL, POST_KERNEL, CONFIG_LED_INIT_PRIORITY,
-              &cyw43_led_api);
+              &rpi_cyw43_led_api);
 
